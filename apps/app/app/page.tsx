834819e4b57e101348bd149f5c6d9f11a6b128ff
Daltimore <obinnaleo78@gmail.com>
2026-04-14 19:02:04 +0100

    bug fixes

--- apps/app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">Vestrapay App</h1>
-    </main>
-  );
-}
